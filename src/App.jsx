f585b5b98d6893675809991c17ea2d944f25f5f5
Raushan Kumar <raushankp75@gmail.com>
2023-03-21 19:49:24 +0530

    Create vite+react app with tailwind css and also create the folder structure for starter the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+        <h1 className='text-4xl font-extrabold text-blue-700 text-center'>Task Tracker List</h1>
+    </div>
+  )
+}
+
+export default App
