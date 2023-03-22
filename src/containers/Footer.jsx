5635e25fb45e1db74f164a7e71a93269a5cf2c86
Raushan Kumar <raushankp75@gmail.com>
2023-03-22 23:17:53 +0530

    Added users list in Header and Changing CreateUser Image upload icon

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='h-[5vh] bg-gray-700 flex items-center justify-center text-white text-xl'>
+    <footer className='h-[5vh] bg-gray-700 flex items-center justify-center text-white text-sm'>
         Copyright &#169; 2023 All rights are resereved by Codebucket solutions Pvt. Ltd.
     </footer>
   )
--- src/pages/users/UserList.jsx
+++ src/pages/users/UserList.jsx
@@ -14,20 +14,20 @@ const UserList = () => {
     setPopup(true)
   }
   return (
-    <div className='flex flex-col h-[85vh] w-full gap-10 mx-16 '>
+    <div className='flex flex-col h-[85vh] w-full gap-10 '>
 
       {popup && <CreateUser popup={setPopup} />}
-      <div className='flex flex-row justify-between gap-x-5 px-12 my-5'>
+      <div className='flex flex-row justify-between gap-x-5 px-16 py-8'>
         <div>
-          <input type="text" placeholder='Search...' className='outline outline-slate-400 rounded-md px-3 py-3 w-[400px] focus:outline-2 focus:outline-blue-500' />
+          <input type="text" placeholder='Search...' className='outline outline-slate-400 rounded-md px-3 py-2 w-[400px] focus:outline-2 focus:outline-blue-500' />
         </div>
 
         <button type='button' onClick={handlePopup} className=' w-fit px-6 py-2 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer text-gray-100 font-bold text-xl hover:duration-500 hover:scale-95'>Create User</button>
       </div>
 
-{/* userlist */}
-      <div className='grid grid-cols-4 gap-5 '>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
+      {/* userlist */}
+      <div className='grid grid-cols-4 gap-5 overflow-y-auto overflow-x-hidden py-2 px-16'>
+        <div className='flex flex-row gap-x-5 bg-gray-300 p-5 rounded-lg'>
           <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
           <div>
             <h1 className='text-xl font-semibold'>Rakesh</h1>
@@ -47,7 +47,7 @@ const UserList = () => {
             </div>
           </div>
         </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
+        <div className='flex flex-row gap-x-5 bg-gray-300 p-5 rounded-lg'>
           <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
           <div>
             <h1 className='text-xl font-semibold'>Rakesh</h1>
@@ -67,7 +67,7 @@ const UserList = () => {
             </div>
           </div>
         </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
+        <div className='flex flex-row gap-x-5 bg-gray-300 p-5 rounded-lg'>
           <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
           <div>
             <h1 className='text-xl font-semibold'>Rakesh</h1>
@@ -87,7 +87,7 @@ const UserList = () => {
             </div>
           </div>
         </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
+        <div className='flex flex-row gap-x-5 bg-gray-300 p-5 rounded-lg'>
           <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
           <div>
             <h1 className='text-xl font-semibold'>Rakesh</h1>
@@ -107,7 +107,7 @@ const UserList = () => {
             </div>
           </div>
         </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
+        <div className='flex flex-row gap-x-5 bg-gray-300 p-5 rounded-lg'>
           <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
           <div>
             <h1 className='text-xl font-semibold'>Rakesh</h1>
@@ -127,7 +127,7 @@ const UserList = () => {
             </div>
           </div>
         </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
+        <div className='flex flex-row gap-x-5 bg-gray-300 p-5 rounded-lg'>
           <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
           <div>
             <h1 className='text-xl font-semibold'>Rakesh</h1>
@@ -147,246 +147,7 @@ const UserList = () => {
             </div>
           </div>
         </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
 
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row gap-x-5 bg-gray-200 p-5'>
-          <img src="/src/assets/cb.jpg" alt="User Image" className='w-20 h-20 rounded-full border-2' />
-          <div>
-            <h1 className='text-xl font-semibold'>Rakesh</h1>
-            <p>rakesh@gmail.com</p>
-            <p>8548459658</p>
-          </div>
-
-          <div className='flex flex-col gap-y-3 w-12'>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BsListTask className='text-red-600' /> <span>1</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <FcProcess className='text-green-600' /> <span>5</span>
-            </div>
-            <div className='flex flex-row justify-between items-center gap-x-2'>
-              <BiTask className='text-blue-600' /> <span>15</span>
-            </div>
-          </div>
-        </div>
 
         
       </div>
